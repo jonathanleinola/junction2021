e3e1fe5c8aa4cc3d23091763fc751e30b41191c5
Mikael Metsälä <mikael.metsala@gmail.com>
2021-11-21 01:15:45 +0200

    added spend_data_combined.csv and fixed bubble charts

--- render_plots.py
+++ render_plots.py
@@ -3,8 +3,7 @@ import plotly.express as px
 
 # Render the data
 df1 = pd.read_csv("data/emission_per_eur.csv")
-df2 = pd.read_csv("data/spend_data.csv")
-df3 = pd.read_csv("data/spend_data.csv")
+df2 = pd.read_csv("data/preprocessed/spend_data_combined.csv")
 
 
 def plot1():
@@ -26,14 +25,15 @@ def plot2():
 
 def plot3():
     bubble_size = []
-    bubble_size = df2.iloc[:, 2]
+    bubble_size = df2.iloc[:,15]
 
 
-    fig = px.scatter(df2, x="SpendEUR", y="Quantity",
-                     size="SpendEUR", color="CategoryL2",
+    fig = px.scatter(df2, x="Quantity", y="SpendEUR",
+                     size="total_CO2_for_purchase", color="CategoryL2",
                      hover_name="ProductName", hover_data=['ProductName', 'SpendEUR', 'Quantity',
-                     'VendorCity', 'VendorCountry', 'CategoryL1', 'CategoryL2', 'VendorName'], log_x=True, log_y=True, size_max=60)
-    fig.update_traces(mode='markers', marker=dict(sizeref= 2.*max(bubble_size)/(60**2), line_width=2))
+                     'VendorCity', 'VendorCountry', 'CategoryL1', 'CategoryL2', 'VendorName'],
+                     size_max=60, log_x=True)
+    fig.update_traces(mode='markers', marker=dict(sizeref= 2.*max(bubble_size)/(100**2)))
 
     return fig
 
